1da0e15a7c672ed42849300940b7f0838882b8e0
Dhruv Shah <dhruvshahrds@gmail.com>
2021-10-16 23:12:39 +0530

    feat: made reminders better

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import json
 import requests
 from random import randint
-
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -17,7 +17,11 @@ WAPI = os.getenv("WEATHERAPI")
 
 
 f=open("reminders.txt")
-reminders=(eval(f.read()))
+Data=eval(f.read())
+reminders=Data["reminders"]
+#{"date and time":[user.id, task]}
+user_reminders=Data["user reminders"]
+#{user.id:["date and time"]}
 f.close()
 f=open("xps.txt")
 xps=eval(f.read())
@@ -27,7 +31,8 @@ f.close()
 def commit(t):
     if t=="r":
         f=open("reminders.txt","w")
-        f.write(str(reminders))
+        Data={"reminders":reminders, "user reminders":user_reminders }
+        f.write(str(Data))
         f.close()
     else:
         f=open("xps.txt","w")
@@ -87,6 +92,28 @@ async def on_ready():
 
 
 @bot.command()
+async def init(ctx):
+    await ctx.trigger_typing()
+    msg= await ctx.send("```Initializing.```")
+    for i in range(10):
+
+        await msg.edit(content="```Initializing..```")
+
+        await msg.edit(content="```Initializing...```")
+
+        await msg.edit(content="```Initializing.```")
+
+    await msg.edit(content="```Initializing..```")
+
+    await msg.edit(content="```Initializing...```")
+
+    await msg.edit(content="```Initialized successfully!```")
+
+
+
+
+
+@bot.command()
 async def top(ctx):
     text="```py\n📋 Rank | Name \n\n"
     values=[tuple(x) for x in xps.values()]
@@ -103,81 +130,156 @@ async def top(ctx):
     await ctx.send(content=text)
 
 
-
 @tasks.loop(minutes=1)
 async def check(reminders):
     for i in reminders:
-        if datetime.now().strftime("%d-%m-%Y %I:%M %p")==i:
+        if datetime.now().strftime("%d-%m-%y %I.%M %p")>=i:
             for j in reminders[i]:
                 user=bot.get_user(j[0])
-                await user.send("You have a reminder for the task : "+j[1])
+                await user.send(f"You have a reminder for the task : ```{j[1]}```")
             reminders.pop(i)
             commit("r")
             break
 
 
-@bot.command(aliases=["remind_me","R"])
-async def remind(ctx):
 
-    def check(m):
-        return m.channel==ctx.channel and m.author==ctx.author
-    format="%d-%m-%Y"
-
+@bot.command(aliases=["remind_me","R","remind","remindme"])
+async def setreminder(ctx, date=None,*,time="12.00 AM"):
     try:
-        await ctx.send("Enter task date(dd-mm-yyyy) : ")
-        date = await bot.wait_for("message", check=check, timeout=120)
-        try:
-            datetime.strptime(date.content,format)
-        except ValueError:
-            await ctx.send("Incorrect format of date, please try again!")
-    except Exception:
-        await ctx.send("No reply, session timed out!")
+        if date==None:
+            Embed=discord.Embed(title="ERROR !",
+                                description="Enter a date please! Please try again using `!r setreminder`",
+                                colour=0xdb1414)
+            await ctx.send(embed=Embed)
+            return
+        elif date.lower()=="today":
+            date=datetime.now().strftime("%d-%m-%y")
 
-    try:
-        await ctx.send("Enter time(hh:mm AM/PM): ")
-        time = await bot.wait_for("message", check=check, timeout=120)
-        try:
-            datetime.strptime(time.content.upper(),"%I:%M %p")
-        except ValueError:
-            await ctx.send("Incorrect format of time, please try again!")
-        print(date.content+time.content)
-    except Exception:
-        await ctx.send("No reply, session timed out!")
 
-    try:
-        await ctx.send("Enter task : ")
-        task = await bot.wait_for("message", check=check, timeout=180)
-    except Exception:
-        await ctx.send("No reply, session timed out!")
+        time.upper()
+        time=time.replace(":",".")
+        date=date.replace("/","-")
+        date=date.replace(".","-")
+        format="%d-%m-%y %I.%M %p"
+        dt=date+" "+time
+        print(dt)
+        dcheck=datetime.strptime(dt,format)
+        if datetime.now()>=dcheck:
+            Embed=discord.Embed(title="ERROR !",
+                                description="Date and time has already passed! Please try again using `!r setreminder`",
+                                colour=0xdb1414)
+            await ctx.send(embed=Embed)
+            return
 
-    dt=date.content+" "+time.content
-    print(dt)
-    if dt not in reminders:
-        reminders[dt]=[[ctx.author.id,task.content]]
-    else:
-        reminders[dt].append([ctx.author.id,task.content])
-    print(reminders)
+        dt=dcheck.strftime(format)
+        def check(m):
+            return m.channel==ctx.channel and m.author==ctx.author
 
-    await ctx.send("Reminder added!")
-    commit("r")
+        await ctx.send("```Please enter Task : ```")
+        task = await bot.wait_for("message", check=check, timeout=30)
+
+
+        msg = await ctx.send("```Initializing...```")
+        await ctx.trigger_typing()
+        await asyncio.sleep(2)
+
+        if dt not in reminders:
+            reminders[dt]=[[ctx.author.id, task.content]]
+
+        else:
+            reminders[dt].append([ctx.author.id, task.content])
+
+
+        if ctx.author.id not in user_reminders:
+            user_reminders[ctx.author.id]=[dt]
+        else:
+            user_reminders[ctx.author.id].append(dt)
+
+
+        await ctx.trigger_typing()
+        await msg.edit(content="```Fetching reminder details..```")
+        await asyncio.sleep(2)
+        await msg.edit(content=f"```Adding reminder for date {date}...```")
+        await asyncio.sleep(2)
+
+        commit("r")
+
+        Embed=discord.Embed(title="REMINDER ADDED ✅",
+                            description="Reminder added successfully. Please do !r reminderlist for more.",
+                            colour=0x2bf707)
+        Embed.add_field(name="Date", value=date)
+        Embed.add_field(name="Time", value=time)
+        Embed.add_field(name="Task", value=f"```{task.content}```", inline=False)
+        await msg.edit(content="", embed=Embed)
+
+    except ValueError:
+        Embed=discord.Embed(title="ERROR!",
+                            description="Invalid format of date or time. Correct format-\n ```Date - dd-mm-yy \nTime - hh.mm AM/PM \n\nExample - !r setreminder 19-06-21 12:00 AM```",
+                            colour=0xdb1414)
+        await ctx.send(embed=Embed)
+        return
+    except asyncio.TimeoutError:
+        Embed=discord.Embed(title="Error!",
+                            description="Session timed out! Please try again using `!r setreminder`",
+                            colour=0xdb1414)
+        await ctx.send(embed=Embed)
+
+
+
+
+
+
+
+
 
 
 
 
 @bot.command(aliases=["Reminder_list","reminder_list"])
-async def rl(ctx):
-    Embed=discord.Embed(title="REMINDERS",description="All the reminders based on date-")
-    dt=list(reminders)
-    dt.sort()
+async def rl(ctx, user : discord.Member = None):
+    if user==None:
+        user = ctx.author
+    if user.id not in user_reminders:
+        Embed=discord.Embed(title="ERROR !",
+                            description="You don't have any reminders! To add reminder use - `!r setreminder`",
+                            colour=0xdb1414)
+        await ctx.send(embed=Embed)
+        return
 
-    for i in dt:
-        val=""
-        for j in reminders[i]:
-            user=bot.get_user(j[0])
-            val+=f"{j[1]} - added by `{user.name}`\n"
-        Embed.add_field(name=i, value=val, inline=False)
+    reminderdates=user_reminders[user.id]
+
+
+
+    Embed=discord.Embed(title="REMINDERS 📅", description=f"Reminders of {user.name}")
+    for i in reminderdates:
+        data = reminders[i]
+        data.sort()
+        sno=0
+        for j in data:
+            text=""
+            if j[0]==user.id:
+                sno+=1
+                text+=f"{sno}. **{j[1]}** - added by `{user.name}`\n"
+
+        Embed.add_field(name=f"{i}", value=text, inline=False)
 
     await ctx.send(embed=Embed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
